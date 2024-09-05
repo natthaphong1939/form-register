379ea07fdbec8445cb8ff0918256b653d7eac10c
dew <natthaphong.t@kkumail.com>
2024-09-05 19:20:41 +0700

    Update registrations controller to handle form submissions

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,20 @@
 class RegistrationsController < ApplicationController
   def index
-    @registration = Registration.new
+    @registration = Registration.all
+  end
+
+  def create
+    @registration = Registration.new(registration_params)
+    if @registration.save
+      redirect_to root_path, notice: "Thank you for registering!"
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def registration_params
+    params.require(:user).permit(:first_name, :last_name, :birthday, :gender, :email, :phone_number, :subject)
   end
 end
