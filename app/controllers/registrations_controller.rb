e7f9dd2571f5b52e9e002d1fbc7ce138b2565fe0
dew <natthaphong.t@kkumail.com>
2024-09-05 23:00:32 +0700

    Update registrations controller and views to support new and show actions

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,20 +1,27 @@
 class RegistrationsController < ApplicationController
   def index
-    @registration = Registration.all
+  end
+
+  def new
+    @registration = Registration.new
+  end
+
+  def show
+    @registrations = Registration.all
   end
 
   def create
     @registration = Registration.new(registration_params)
     if @registration.save
-      redirect_to root_path, notice: "Thank you for registering!"
+      redirect_to registrations_url(@registration)
     else
       render :index
     end
   end
-
+  
   private
 
   def registration_params
-    params.require(:user).permit(:first_name, :last_name, :birthday, :gender, :email, :phone_number, :subject)
+    params.require(:registration).permit(:first_name, :last_name, :birthday, :gender, :email, :phone_number, :subject)
   end
 end
